c27dd95f85319e2998406cf31b8e3a778393fa49
Andres Parra <acpdelgadillo@gmail.com>
2025-05-17 22:06:54 -0500

    feat: add search in subjects

---
+++ js/subjects_data.js
@@ -0,0 +1,34 @@
+const subjectsData = [
+    {
+        name: "Probabilidad y estadística inferencial",
+        credits: 3,
+        duration: "Ciclo"
+    },
+    {
+        name: "Programación orientada por objetos",
+        credits: 4,
+        duration: "Semestre"
+    },
+    {
+        name: "Redes I",
+        credits: 3,
+        duration: "Semestre"
+    },
+    {
+        name: "Ecuaciones diferenciales",
+        credits: 3,
+        duration: "Ciclo"
+    },
+    {
+        name: "Matemáticas discretas",
+        credits: 4,
+        duration: "Semestre"
+    },
+    {
+        name: "Matemáticas prueeba",
+        credits: 4,
+        duration: "Semestre"
+    }
+];
+
+export default subjectsData; 
